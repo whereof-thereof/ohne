3c8e6218f921788f7eff0eda34f404a2a02750ba
Rob Safar <rob.safar@gmail.com>
2016-04-29 14:53:43 +0100

    A start.

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # ohne
 
-Ohne minimal WordPress theme
+Ohne brutalist WordPress theme.
 
-Believe.
+**This theme is not currently ready.**
+
+By [@robsafar](https:twitter.com/robsafar). Base theme is [HTML5blank](http://html5blank.com/). Fonts are [Source Sans Pro](https://adobe-fonts.github.io/source-sans-pro/) and [Source Code Pro](https://adobe-fonts.github.io/source-code-pro/).
+
+## Considerations
+
+- Not comment- or social-media-button-friendly.
+- `border-bottom`s are used for links, so not underline-friendly.
--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+		<link href='https://fonts.googleapis.com/css?family=Source+Code+Pro|Source+Sans+Pro:400,700,400italic' rel='stylesheet' type='text/css'>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
@@ -32,12 +33,11 @@
 			<header class="header clear" role="banner">
 
 					<!-- logo -->
-					<div class="logo">
+					<h1 class="logo">
 						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							<?php bloginfo('name'); ?>
 						</a>
-					</div>
+					</h1>
 					<!-- /logo -->
 
 					<!-- nav -->
